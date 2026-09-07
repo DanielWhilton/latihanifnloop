f55fd465a24f5d350b7611447cf46833f50d9860
DanielWhilton <rexagonium@gmail.com>
2026-09-07 19:36:58 +0700

    first commit

---
+++ latihanifnloop.dart
@@ -0,0 +1,45 @@
+void main(){
+
+    print('Hello World');
+    // int kehadiran = 75;
+    // if (kehadiran < 75) {
+    //     print('Tidak bisa ikut ujian');
+    // } else {
+    //     print('Bisa ikut ujian');
+    // }
+
+    // if (kehadiran < 75) {
+    //     print('Tidak bisa ikut ujian');
+    // }
+
+    // if (kehadiran >= 75) {
+    //     print('Bisa ikut ujian');
+    // }
+
+    // var nilaiakhir = 'A';
+    // switch (nilaiakhir) {
+    //     case 'A':
+    //         print('Nilai sangat memuaskan');
+    //         break;
+    //     case 'B':
+    //         print('Nilai memuaskan');
+    //         break;
+    //     default:
+    //         print('Nilai tidak dalam cakupan');
+    // }
+
+
+    //perulangan
+    // List datamhs = ['Andri','Merry','Windy','Onad','Jesika'];
+    // for (var i = 0; i < datamhs.length; i++) { //nilai awal, batas, increment
+    //     print(datamhs[i]);
+    // }
+
+    // for (var element in collection) {
+    // }
+
+    // foreach
+
+
+
+}
