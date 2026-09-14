1db6a5361b3acd5aeed1ba25b443f78d4e7d1063
DanielWhilton <rexagonium@gmail.com>
2026-09-14 08:57:41 +0700

    first commit

---
+++ coba.dart
@@ -0,0 +1,6 @@
+import 'dart:io';
+void main(){
+  stdout.writeIn('Masukkan angka 1 anda');
+  var inputUser1 = stdin.readLineSync();
+
+}
--- latihanifnloop.dart
+++ latihanifnloop.dart
@@ -1,6 +1,6 @@
 void main(){
 
-    print('Hello World');
+    // print('Hello World');
     // int kehadiran = 75;
     // if (kehadiran < 75) {
     //     print('Tidak bisa ikut ujian');
@@ -40,6 +40,11 @@ void main(){
 
     // foreach
 
+  //Tugas 1
+  //1. Menghitung penjumlahan 5 variabel dengan tipe data yang berbeda
+  //2. Menghitung luas dan keliling lingkaran dengan penerapan tipe data final atau const
+  //3. Manipulasi string dengan membuat paragraf sederhana dimana:
+  //
 
 
 }
